7a8fce9c6535372baf8224c2a86eb8cb2cd29ad0
Anastasia-Galyamina <anastasiagalyamina@github.com>
2019-10-25 15:28:52 +0400

    лабораторная 3

---
+++ WindowsFormsCars/WindowsFormsCars/Properties/FormParking.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsCars
+{
+    public partial class FormParking : Form
+    {
+        /// <summary>
+        /// Объект от класса-парковки
+        /// </summary>
+        Parking<ITransport> parking;
+        public FormParking()
+        {
+            InitializeComponent();
+            parking = new Parking<ITransport>(20, pictureBoxParking.Width,
+           pictureBoxParking.Height);
+            Draw();
+        }
+
+        /// <summary>
+        /// Метод отрисовки парковки
+        /// </summary>
+        private void Draw()
+        {
+            Bitmap bmp = new Bitmap(pictureBoxParking.Width, pictureBoxParking.Height);
+            Graphics gr = Graphics.FromImage(bmp);
+            parking.Draw(gr);
+            pictureBoxParking.Image = bmp;
+        }
+        /// <summary>
+        /// Обработка нажатия кнопки "Припарковать автомобиль"
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonSetShip_Click(object sender, EventArgs e)
+        {
+            ColorDialog dialog = new ColorDialog();
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                var ship = new Ship(100, 1000, dialog.Color);
+                int place = parking + ship;
+                Draw();
+            }
+        }
+
+        /// <summary>
+        /// Обработка нажатия кнопки "Припарковать гоночный автомобиль"
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonSetMotorShip_Click(object sender, EventArgs e)
+        {
+            ColorDialog dialog = new ColorDialog();
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                ColorDialog dialogDop = new ColorDialog();
+                if (dialogDop.ShowDialog() == DialogResult.OK)
+                {
+                    var ship = new MotorShip(100, 1000, dialog.Color, dialogDop.Color, true);
+                    int place = parking + ship;
+                    Draw();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Обработка нажатия кнопки "Забрать"
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonTakeShip_Click(object sender, EventArgs e)
+        {
+            if (maskedTextBox.Text != "")
+            {
+                var car = parking - Convert.ToInt32(maskedTextBox.Text);
+                if (car != null)
+                {
+                    Bitmap bmp = new Bitmap(pictureBoxTakeShip.Width,
+                   pictureBoxTakeShip.Height);
+                    Graphics gr = Graphics.FromImage(bmp);
+                    car.SetPosition(5, 5, pictureBoxTakeShip.Width,
+                   pictureBoxTakeShip.Height);
+                    car.DrawShip(gr);
+                    pictureBoxTakeShip.Image = bmp;
+                }
+                else
+                {
+                    Bitmap bmp = new Bitmap(pictureBoxTakeShip.Width,
+                   pictureBoxTakeShip.Height);
+                    pictureBoxTakeShip.Image = bmp;
+                }
+                Draw();
+            }
+        }
+
+       
+    }
+}

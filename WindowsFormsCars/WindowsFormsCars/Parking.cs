80b4d1e4454646350286478b13b2736b00a5e6d4
Anastasia-Galyamina <anastasiagalyamina@github.com>
2019-11-15 09:58:31 +0400

    lab4

--- WindowsFormsCars/WindowsFormsCars/Parking.cs
+++ WindowsFormsCars/WindowsFormsCars/Parking.cs
@@ -14,6 +14,10 @@ namespace WindowsFormsCars
         /// </summary>
         private T[] _places;
         /// <summary>
+        /// Максимальное количество мест на парковке
+        /// </summary>
+        private int _maxCount;
+        /// <summary>
         /// Ширина окна отрисовки
         /// </summary>
         private int PictureWidth { get; set; }
@@ -29,63 +33,82 @@ namespace WindowsFormsCars
         /// Размер парковочного места (высота)
         /// </summary>
         private const int _placeSizeHeight = 80;
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        /// <param name="sizes">Количество мест на парковке</param>
+        /// <param name="pictureWidth">Рамзер парковки - ширина</param>
+        /// <param name="pictureHeight">Рамзер парковки - высота</param>
         public Parking(int sizes, int pictureWidth, int pictureHeight)
         {
-            _places = new T[sizes];
-            PictureWidth = pictureWidth;
-            PictureHeight = pictureHeight;
-            for (int i = 0; i < _places.Length; i++)
-            {
-                _places[i] = null;
-            }
+        _maxCount = sizes;
+        _places = new Dictionary<int, T>();
+        PictureWidth = pictureWidth;
+        PictureHeight = pictureHeight;
         }
+        /// <summary>
+        /// Перегрузка оператора сложения
+        /// Логика действия: на парковку добавляется автомобиль
+        /// </summary>
+        /// <param name="p">Парковка</param>
+        /// <param name="car">Добавляемый автомобиль</param>
+        /// <returns></returns>
         public static int operator +(Parking<T> p, T car)
         {
-            for (int i = 0; i < p._places.Length; i++)
+            if (p._places.Count == p._maxCount)
+            {
+                return -1;
+            }
+            for (int i = 0; i < p._maxCount; i++)
             {
                 if (p.CheckFreePlace(i))
                 {
-                    p._places[i] = car;
+                    p._places.Add(i, car);
                     p._places[i].SetPosition(5 + i / 5 * _placeSizeWidth + 5,
-                     i % 5 * _placeSizeHeight + 15, p.PictureWidth,
+                    i % 5 * _placeSizeHeight + 15, p.PictureWidth,
                     p.PictureHeight);
                     return i;
                 }
             }
             return -1;
         }
+        /// <summary>
+        /// Перегрузка оператора вычитания
+        /// Логика действия: с парковки забираем автомобиль
+        /// </summary>
+        /// <param name="p">Парковка</param>
+        /// <param name="index">Индекс места, с которого пытаемся извлечь объект</param>
+        /// <returns></returns>
         public static T operator -(Parking<T> p, int index)
         {
-            if (index < 0 || index > p._places.Length)
-            {
-                return null;
-            }
             if (!p.CheckFreePlace(index))
             {
                 T car = p._places[index];
-                p._places[index] = null;
+                p._places.Remove(index);
                 return car;
             }
             return null;
         }
+        /// <summary>
+        /// Метод проверки заполнености парковочного места (ячейки массива)
+        /// </summary>
+        /// <param name="index">Номер парковочного места (порядковый номер в массиве)</param>
+        /// <returns></returns>
         private bool CheckFreePlace(int index)
         {
-            return _places[index] == null;
+            return !_places.ContainsKey(index);
         }
-
         /// <summary>
         /// Метод отрисовки парковки
         /// </summary>
-        /// <param name="g"></param>
+        /// /// <param name="g"></param>
         public void Draw(Graphics g)
         {
             DrawMarking(g);
-            for (int i = 0; i < _places.Length; i++)
+            var keys = _places.Keys.ToList();
+            for (int i = 0; i < keys.Count; i++)
             {
-                if (!CheckFreePlace(i))
-                {//если место не пустое
-                    _places[i].DrawShip(g);
-                }
+                _places[keys[i]].DrawCar(g);
             }
         }
         /// <summary>
@@ -96,11 +119,13 @@ namespace WindowsFormsCars
         {
             Pen pen = new Pen(Color.Black, 3);
             //границы праковки
-            g.DrawRectangle(pen, 0, 0, (_places.Length / 5) * _placeSizeWidth, 480);
-            for (int i = 0; i < _places.Length / 5; i++)
-            {//отрисовываем, по 5 мест на линии
+            g.DrawRectangle(pen, 0, 0, (_maxCount / 5) * _placeSizeWidth, 480);
+            for (int i = 0; i < _maxCount / 5; i++)
+            {
+                //отрисовываем, по 5 мест на линии
                 for (int j = 0; j < 6; ++j)
-                {//линия рамзетки места
+                {
+                    //линия рамзетки места
                     g.DrawLine(pen, i * _placeSizeWidth, j * _placeSizeHeight,
                     i * _placeSizeWidth + 110, j * _placeSizeHeight);
                 }

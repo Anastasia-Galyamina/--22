fbee3ce8f187f3b63754b5b6787c4d9d46b29ce6
Anastasia-Galyamina <anastasiagalyamina@github.com>
2019-11-22 16:07:51 +0400

    Revert "лаба2"
    This reverts commit 8e537c6edd8df70aac8ca0b2345b3f5e80ee9114, reversing
    changes made to 4302688b9281126471984193d64bb559c36e9364.

--- WindowsFormsCars/WindowsFormsCars/FormShip.Designer.cs
+++ WindowsFormsCars/WindowsFormsCars/FormShip.Designer.cs
@@ -8,6 +8,7 @@ namespace WindowsFormsCars
         /// Обязательная переменная конструктора.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
+
         /// <summary>
         /// Освободить все используемые ресурсы.
         /// </summary>
@@ -36,7 +37,6 @@ namespace WindowsFormsCars
             this.buttonDown = new System.Windows.Forms.Button();
             this.buttonLeft = new System.Windows.Forms.Button();
             this.buttonRight = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxShip)).BeginInit();
             this.SuspendLayout();
             // 
@@ -51,11 +51,11 @@ namespace WindowsFormsCars
             // 
             // buttonCreate
             // 
-            this.buttonCreate.Location = new System.Drawing.Point(552, 234);
+            this.buttonCreate.Location = new System.Drawing.Point(708, 235);
             this.buttonCreate.Name = "buttonCreate";
-            this.buttonCreate.Size = new System.Drawing.Size(146, 38);
+            this.buttonCreate.Size = new System.Drawing.Size(108, 38);
             this.buttonCreate.TabIndex = 1;
-            this.buttonCreate.Text = "Создать корабль";
+            this.buttonCreate.Text = "Создать";
             this.buttonCreate.UseVisualStyleBackColor = true;
             this.buttonCreate.Click += new System.EventHandler(this.buttonCreate_Click);
             // 
@@ -103,22 +103,11 @@ namespace WindowsFormsCars
             this.buttonRight.UseVisualStyleBackColor = true;
             this.buttonRight.Click += new System.EventHandler(this.buttonMove_Click);
             // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(720, 234);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(146, 38);
-            this.button1.TabIndex = 6;
-            this.button1.Text = "Создать теплоход";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.buttonCreateMotorShip_Click);
-            // 
             // FormShip
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(882, 455);
-            this.Controls.Add(this.button1);
             this.Controls.Add(this.buttonRight);
             this.Controls.Add(this.buttonLeft);
             this.Controls.Add(this.buttonDown);
@@ -146,7 +135,6 @@ namespace WindowsFormsCars
         private System.Windows.Forms.Button buttonDown;
         private System.Windows.Forms.Button buttonLeft;
         private System.Windows.Forms.Button buttonRight;
-        private System.Windows.Forms.Button button1;
     }
 }
 

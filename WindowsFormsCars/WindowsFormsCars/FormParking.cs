dcf797dee3c5b8d1339a724b617107db1f817dc6
Anastasia-Galyamina <anastasiagalyamina@github.com>
2019-12-06 16:53:52 +0400

    laba4

--- WindowsFormsCars/WindowsFormsCars/FormParking.cs
+++ WindowsFormsCars/WindowsFormsCars/FormParking.cs
@@ -13,25 +13,40 @@ namespace WindowsFormsCars
     public partial class FormParking : Form
     {
         /// <summary>
-        /// Объект от класса-парковки
+        /// Объект от класса многоуровневой парковки
         /// </summary>
-        Parking<ITransport> parking;
+         MultiLevelParking parking;
+        /// <summary>
+        /// Количество уровней-парковок
+        /// </summary>
+        private const int countLevel = 5;
         public FormParking()
         {
             InitializeComponent();
-            parking = new Parking<ITransport>(20, pictureBoxParking.Width,
+            parking = new MultiLevelParking(countLevel, pictureBoxParking.Width,
             pictureBoxParking.Height);
-            Draw();
+            //заполнение listBox
+            for (int i = 0; i < countLevel; i++)
+            {
+                listBoxLevels.Items.Add("Уровень " + (i + 1));
+            }
+            listBoxLevels.SelectedIndex = 0;
         }
         /// <summary>
         /// Метод отрисовки парковки
         /// </summary>
         private void Draw()
         {
-            Bitmap bmp = new Bitmap(pictureBoxParking.Width, pictureBoxParking.Height);
+           if (listBoxLevels.SelectedIndex > -1)
+           {
+            /*если выбран один из пуктов в listBox (при старте программы ни один пункт не будет выбран
+             и может возникнуть ошибка, если мы попытаемся обратиться к элементу listBox)*/
+            Bitmap bmp = new Bitmap(pictureBoxParking.Width,
+            pictureBoxParking.Height);
             Graphics gr = Graphics.FromImage(bmp);
-            parking.Draw(gr);
+            parking[listBoxLevels.SelectedIndex].Draw(gr);
             pictureBoxParking.Image = bmp;
+           }
         }
         /// <summary>
         /// Обработка нажатия кнопки "Припарковать корабль"
@@ -40,12 +55,20 @@ namespace WindowsFormsCars
         /// <param name="e"></param>
         private void buttonSetShip_Click(object sender, EventArgs e)
         {
-            ColorDialog dialog = new ColorDialog();
-            if (dialog.ShowDialog() == DialogResult.OK)
+            if (listBoxLevels.SelectedIndex > -1)
             {
-                var ship = new Ship(100, 1000, dialog.Color);
-                int place = parking + ship;
-                Draw();
+                 ColorDialog dialog = new ColorDialog();
+                 if (dialog.ShowDialog() == DialogResult.OK)
+                 {
+                     var car = new Ship(100, 1000, dialog.Color);
+                     int place = parking[listBoxLevels.SelectedIndex] + car;
+                     if (place == -1)
+                     {
+                        MessageBox.Show("Нет свободных мест", "Ошибка",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                     }
+                     Draw();
+                 }
             }
         }
         /// <summary>
@@ -55,19 +78,27 @@ namespace WindowsFormsCars
         /// <param name="e"></param>
         private void buttonSetMotorShip_Click(object sender, EventArgs e)
         {
-            ColorDialog dialog = new ColorDialog();
-            if (dialog.ShowDialog() == DialogResult.OK)
+            if (listBoxLevels.SelectedIndex > -1)
             {
-                ColorDialog dialogDop = new ColorDialog();
-                if (dialogDop.ShowDialog() == DialogResult.OK)
+                ColorDialog dialog = new ColorDialog();
+                if (dialog.ShowDialog() == DialogResult.OK)
                 {
-                    var ship = new MotorShip(100, 1000, dialog.Color, dialogDop.Color, true);
-                    int place = parking + ship;
-                    Draw();
+                     ColorDialog dialogDop = new ColorDialog();
+                     if (dialogDop.ShowDialog() == DialogResult.OK)
+                     {
+                        var car = new MotorShip(100, 1000, dialog.Color,
+                        dialogDop.Color, true);
+                        int place = parking[listBoxLevels.SelectedIndex] + car;
+                        if (place == -1)
+                        {
+                            MessageBox.Show("Нет свободных мест", "Ошибка",
+                            MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                        Draw();
+                     }
                 }
             }
         }
-
         /// <summary>
         /// Обработка нажатия кнопки "Забрать корабль"
         /// </summary>
@@ -75,27 +106,40 @@ namespace WindowsFormsCars
         /// <param name="e"></param>
         private void buttonTakeShip_Click(object sender, EventArgs e)
         {
-            if (maskedTextBox.Text != "")
+            if (listBoxLevels.SelectedIndex > -1)
             {
-                var car = parking - Convert.ToInt32(maskedTextBox.Text);
-                if (car != null)
-                {
-                    Bitmap bmp = new Bitmap(pictureBoxTakeShip.Width,
-                    pictureBoxTakeShip.Height);
-                    Graphics gr = Graphics.FromImage(bmp);
-                    car.SetPosition(5, 5, pictureBoxTakeShip.Width,
-                    pictureBoxTakeShip.Height);
-                    car.DrawShip(gr);
-                    pictureBoxTakeShip.Image = bmp;
-                }
-                else
-                {
-                    Bitmap bmp = new Bitmap(pictureBoxTakeShip.Width,
-                   pictureBoxTakeShip.Height);
-                    pictureBoxTakeShip.Image = bmp;
-                }
-                Draw();
-            }
-        }       
+                 if (maskedTextBox.Text != "")
+                 {
+                     var ship = parking[listBoxLevels.SelectedIndex] -
+                     Convert.ToInt32(maskedTextBox.Text);
+                     if (ship != null)
+                     {
+                         Bitmap bmp = new Bitmap(pictureBoxTakeShip.Width,
+                         pictureBoxTakeShip.Height);
+                         Graphics gr = Graphics.FromImage(bmp);
+                         ship.SetPosition(5, 5, pictureBoxTakeShip.Width,
+                         pictureBoxTakeShip.Height);
+                         ship.DrawShip(gr);
+                         pictureBoxTakeShip.Image = bmp;
+                     }
+                     else
+                     {
+                         Bitmap bmp = new Bitmap(pictureBoxTakeShip.Width,
+                         pictureBoxTakeShip.Height);
+                         pictureBoxTakeShip.Image = bmp;
+                     }
+                     Draw();
+                 }
+             }
+        } 
+        /// <summary>
+        /// Метод обработки выбора элемента на listBoxLevels
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void listBoxLevels_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Draw();
+        }
     }
 }
--- WindowsFormsCars/WindowsFormsCars/Parking.cs
+++ WindowsFormsCars/WindowsFormsCars/Parking.cs
@@ -12,10 +12,14 @@ namespace WindowsFormsCars
         /// <summary>
         /// Массив объектов, которые храним
         /// </summary>
-        private T[] _places;
+        private Dictionary<int, T> _places;
+        /// <summary>
+        /// Максимальное количество мест на парковке
+        /// </summary>
+        private int _maxCount;
         /// <summary>
         /// Ширина окна отрисовки
-        /// </summary>
+        /// </summary>        
         private int PictureWidth { get; set; }
         /// <summary>
         /// Высота окна отрисовки
@@ -31,21 +35,29 @@ namespace WindowsFormsCars
         private const int _placeSizeHeight = 80;
         public Parking(int sizes, int pictureWidth, int pictureHeight)
         {
-            _places = new T[sizes];
+            _maxCount = sizes;
+            _places = new Dictionary<int, T>();
             PictureWidth = pictureWidth;
             PictureHeight = pictureHeight;
-            for (int i = 0; i < _places.Length; i++)
-            {
-                _places[i] = null;
-            }
         }
+        /// <summary>
+        /// Перегрузка оператора сложения
+        /// Логика действия: на парковку добавляется автомобиль
+        /// </summary>
+        /// <param name="p">Парковка</param>
+        /// <param name="car">Добавляемый автомобиль</param>
+        /// <returns></returns>
         public static int operator +(Parking<T> p, T car)
         {
-            for (int i = 0; i < p._places.Length; i++)
+            if (p._places.Count == p._maxCount)
+            {
+                return -1;
+            }
+            for (int i = 0; i < p._maxCount; i++)
             {
                 if (p.CheckFreePlace(i))
                 {
-                    p._places[i] = car;
+                    p._places.Add(i, car);
                     p._places[i].SetPosition(5 + i / 5 * _placeSizeWidth + 5,
                      i % 5 * _placeSizeHeight + 15, p.PictureWidth,
                     p.PictureHeight);
@@ -54,38 +66,43 @@ namespace WindowsFormsCars
             }
             return -1;
         }
+        /// <summary>
+        /// Перегрузка оператора вычитания
+        /// Логика действия: с парковки забираем автомобиль
+        /// </summary>
+        /// <param name="p">Парковка</param>
+        /// <param name="index">Индекс места, с которого пытаемся извлечь объект</param>
+        /// <returns></returns>
         public static T operator -(Parking<T> p, int index)
         {
-            if (index < 0 || index > p._places.Length)
-            {
-                return null;
-            }
             if (!p.CheckFreePlace(index))
             {
                 T car = p._places[index];
-                p._places[index] = null;
+                p._places.Remove(index);
                 return car;
             }
             return null;
         }
+        /// <summary>
+        /// Метод проверки заполнености парковочного места (ячейки массива)
+        /// </summary>
+        /// <param name="index">Номер парковочного места (порядковый номер в массиве)</param>
+        /// <returns></returns>
         private bool CheckFreePlace(int index)
         {
-            return _places[index] == null;
+            return !_places.ContainsKey(index);
         }
-
         /// <summary>
         /// Метод отрисовки парковки
-        /// </summary>
+        /// </summary>        
         /// <param name="g"></param>
         public void Draw(Graphics g)
         {
             DrawMarking(g);
-            for (int i = 0; i < _places.Length; i++)
+            var keys = _places.Keys.ToList();
+            for (int i = 0; i < keys.Count; i++)
             {
-                if (!CheckFreePlace(i))
-                {//если место не пустое
-                    _places[i].DrawShip(g);
-                }
+                _places[keys[i]].DrawShip(g);
             }
         }
         /// <summary>
@@ -96,8 +113,8 @@ namespace WindowsFormsCars
         {
             Pen pen = new Pen(Color.Black, 3);
             //границы праковки
-            g.DrawRectangle(pen, 0, 0, (_places.Length / 5) * _placeSizeWidth, 480);
-            for (int i = 0; i < _places.Length / 5; i++)
+            g.DrawRectangle(pen, 0, 0, (_maxCount / 5) * _placeSizeWidth, 480);
+            for (int i = 0; i < _maxCount / 5; i++)
             {//отрисовываем, по 5 мест на линии
                 for (int j = 0; j < 6; ++j)
                 {//линия рамзетки места
@@ -107,5 +124,6 @@ namespace WindowsFormsCars
                 g.DrawLine(pen, i * _placeSizeWidth, 0, i * _placeSizeWidth, 400);
             }
         }
+
     }
 }

0f870fd41173c9f36ef1205ffad28dc11c6cb46d
Anastasia-Galyamina <52350714+Anastasia-Galyamina@users.noreply.github.com>
2019-12-20 09:43:08 +0400

    Update Parking.cs

--- WindowsFormsCars/WindowsFormsCars/Parking.cs
+++ WindowsFormsCars/WindowsFormsCars/Parking.cs
@@ -51,9 +51,9 @@ namespace WindowsFormsCars
         /// Логика действия: на парковку добавляется корабль
         /// </summary>
         /// <param name="p">Парковка</param>
-        /// <param name="car">Добавляемый корабль</param>
+        /// <param name="ship">Добавляемый корабль</param>
         /// <returns></returns>
-        public static int operator +(Parking<T> p, T car)
+        public static int operator +(Parking<T> p, T ship)
         {
             if (p._places.Count == p._maxCount)
             {
@@ -63,7 +63,7 @@ namespace WindowsFormsCars
             {
                 if (p.CheckFreePlace(i))
                 {
-                    p._places.Add(i, car);
+                    p._places.Add(i, ship);
                     p._places[i].SetPosition(5 + i / 5 * _placeSizeWidth + 5,
                      i % 5 * _placeSizeHeight + 15, p.PictureWidth,
                     p.PictureHeight);
@@ -85,7 +85,7 @@ namespace WindowsFormsCars
             {
                 T car = p._places[index];
                 p._places.Remove(index);
-                return car;
+                return ship;
             }
             throw new ParkingNotFoundException(index);
         }
